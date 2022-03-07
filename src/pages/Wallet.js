e50857c0714292282007a4a0708dc2525721244b
Alan Brazões <alanbrazoes@gmail.com>
2022-03-07 15:29:41 -0300

    header wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  state = {
+    total: 0,
+    courrency: 'BLR',
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { user: { email } } = this.props;
+    const { total, courrency } = this.state;
+    return (
+      <header>
+        <h1 data-testid="email-field">{email}</h1>
+        <p data-testid="total-field">{total}</p>
+        <p data-testid="header-currency-field">{courrency}</p>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Wallet.propTypes = {
+  user: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
